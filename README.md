9dc897a7c93a595d705e98b4818e00517e7a19d5
amacar <amadej.pevec@gmail.com>
2016-10-10 10:31:46 +0200

    First commit
    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+VCF Splitter
+=
+I created this simple program for splitting single .vcf file that I get from Apple iCloud backup into multiple .vcf files. Some other vcf splitters have problems with special characters (čžš) so I write my own VCF Splitter.
+It is very lightweight and don't need installation. You just run vcfSplit.exe and in first step select your *.vcf file and in second step select folder where do you want to save multiple *.vcf.
+
+You can compile and build it by yourself or use provided vcfSplit.exe.
+
+That's it :)
---
+++ vcfsplit/vcfsplit/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace vcfSplit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                string fileName = openFileDialog1.FileName;
+                MessageBox.Show("Choose save directory in next step!");
+                saveInFile(fileName);
+            }
+        }
+
+        void saveInFile(string fileName)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string folderName = folderBrowserDialog1.SelectedPath;
+                string readContents;
+                using (StreamReader streamReader = new StreamReader(fileName))
+                    readContents = streamReader.ReadToEnd();
+                int BeginIndexInString=readContents.IndexOf("BEGIN:VCARD");
+                while (BeginIndexInString != -1)
+                {
+                    int EndIndexInString = readContents.IndexOf("END:VCARD", BeginIndexInString);
+                    string OneVcf = readContents.Substring(BeginIndexInString, EndIndexInString - BeginIndexInString);
+                    int b = OneVcf.IndexOf("FN:");
+                    int a=OneVcf.LastIndexOf("N:",b);
+                    string VcfName = OneVcf.Substring(a,  b - a);
+                    VcfName = Regex.Replace(VcfName, @"N:|;|:|\r|\n", "");
+                    File.WriteAllText(folderName + "\\" + VcfName + ".vcf", OneVcf,Encoding.Default);
+                    BeginIndexInString = readContents.IndexOf("BEGIN:VCARD", EndIndexInString);
+                }
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Donation send to paypal address: amadej.pevec@gmail.com");
+        }
+    }
+}
